ad64ba47b82e375431d15f35038d1bc7fe840ec9
s <s.gmail.com>
2021-07-03 21:50:45 +0530

    maven code added

---
+++ maven/maven-backend/src/test/java/hello/pkg/AppTest.java
@@ -0,0 +1,12 @@
+package hello.pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
